a878a4b89781e89796fba78bbbefb81bb6b9bf49
rachelki <71536809+rachelki@users.noreply.github.com>
2021-05-25 19:57:28 -0700

    Update lab07.py

--- lab07.py
+++ lab07.py
@@ -25,8 +25,8 @@ def isAnagram(str1, str2):
 def twoSum(nums, target):
     newlist = []
     for i in range(0, len(nums)-1):
-        for j in range(i, len(nums)):
-            if nums[i] + nums[j] == target:
+        for j in range(i+1, len(nums)):
+            if (nums[i] + nums[j] == target):
                 newlist.append(i)
                 newlist.append(j)
                 return newlist
